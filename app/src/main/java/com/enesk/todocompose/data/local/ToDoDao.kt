278b96482e45af97673e244e8e15cde722383786
kocerenes <eneskocerr41@gmail.com>
2022-09-08 14:45:57 +0300

    Setup room database

---
+++ app/src/main/java/com/enesk/todocompose/data/local/ToDoDao.kt
@@ -0,0 +1,52 @@
+package com.enesk.todocompose.data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.enesk.todocompose.data.local.entities.ToDoTaskEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllTasks(): Flow<List<ToDoTaskEntity>>
+
+    @Query("SELECT * FROM todo_table WHERE id=:taskId")
+    fun getSelectedTask(taskId: Int): Flow<ToDoTaskEntity>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addTask(toDoTask: ToDoTaskEntity)
+
+    @Update
+    suspend fun updateTask(toDoTask: ToDoTaskEntity)
+
+    @Delete
+    suspend fun deleteTask(toDoTask: ToDoTaskEntity)
+
+    @Query("DELETE FROM todo_table")
+    suspend fun deleteAllTasks()
+
+    @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTaskEntity>>
+
+    @Query("SELECT * FROM todo_table " +
+            "ORDER BY CASE " +
+            "WHEN priority LIKE 'L%' THEN 1 " +
+            "WHEN priority LIKE 'M%' THEN 2 " +
+            "WHEN priority LIKE 'H%' THEN 3 " +
+            "END")
+    fun sortByLowPriority():Flow<List<ToDoTaskEntity>>
+
+    @Query("SELECT * FROM todo_table " +
+            "ORDER BY CASE " +
+            "WHEN priority LIKE 'H%' THEN 1 " +
+            "WHEN priority LIKE 'M%' THEN 2 " +
+            "WHEN priority LIKE 'L%' THEN 3 " +
+            "END")
+    fun sortByHighPriority():Flow<List<ToDoTaskEntity>>
+
+}
