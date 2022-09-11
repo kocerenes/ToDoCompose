8d1c32afc5acbb26a5586b78ab87e4281790802a
kocerenes <eneskocerr41@gmail.com>
2022-09-12 00:37:39 +0300

    Create the dimens and color

--- app/src/main/java/com/enesk/todocompose/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/enesk/todocompose/presentation/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.enesk.todocompose.presentation.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
@@ -7,8 +9,30 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
+//App
+val AppColor = Color(0xFF70A6D1)
+val DarkAppColor = Color(0xFF106891)
+val TopBarContentColor = Color(0xFFE4EBEB)
+
 //Priority
 val HighPriorityColor = Color(0xFFFF4646)
 val MediumPriorityColor = Color(0xFFFFC114)
 val LowPriorityColor = Color(0xFF00C980)
-val NonePriorityColor = Color(0xFFFFFFFF)
+val NonePriorityColor = Color(0xFF999898)
+
+val LightGray = Color(0xFFFCFCFC)
+val MediumGray = Color(0xFF9C9C9c)
+val DarkGray = Color(0xFF141414)
+
+val Colors.fabBackgroundColor: Color
+    @Composable
+    get() = if (isLight) AppColor else DarkAppColor
+
+val Colors.topAppBarContentColor: Color
+    @Composable
+    get() = if (isLight) Color.White else LightGray
+
+val Colors.topAppBarBackgroundColor: Color
+    @Composable
+    get() = if (isLight) AppColor else Color.Black
+
---
+++ app/src/main/java/com/enesk/todocompose/presentation/ui/theme/Dimens.kt
@@ -0,0 +1,25 @@
+package com.enesk.todocompose.presentation.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val DOUBLE_EXTRA_LARGE_PADDING = 24.dp
+val EXTRA_EXTRA_LARGE_PADDING = 20.dp
+val EXTRA_LARGE_PADDING = 18.dp
+val LARGE_PADDING = 16.dp
+val MEDIUM_PADDING = 12.dp
+val SMALL_PADDING = 10.dp
+val EXTRA_SMALL_PADDING = 6.dp
+
+val PRIORITY_INDICATOR_SIZE = 16.dp
+
+val DOUBLE_EXTRA_LARGE_TEXT_FONT_SIZE = 30.sp
+val EXTRA_LARGE_TEXT_FONT_SIZE = 24.sp
+val LARGE_TEXT_FONT_SIZE = 20.sp
+val MEDIUM_TEXT_FONT_SIZE = 18.sp
+val SMALL_TEXT_FONT_SIZE = 16.sp
+val TEXT_FONT_SIZE = 14.sp
+val EXTRA_SMALL_TEXT_FONT_SIZE = 12.sp
+val DOUBLE_EXTRA_SMALL_TEXT_FONT_SIZE = 10.sp
+
+val DEFAULT_TEXT_FONT_SIZE = 16.sp
