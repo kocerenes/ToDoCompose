1339ae6fabde350fd28124d2a4c95cdda603ece1
kocerenes <eneskocerr41@gmail.com>
2022-09-23 17:22:34 +0300

    Create the display snack bar

--- app/src/main/java/com/enesk/todocompose/presentation/screens/list/ListScreen.kt
+++ app/src/main/java/com/enesk/todocompose/presentation/screens/list/ListScreen.kt
@@ -1,24 +1,29 @@
 package com.enesk.todocompose.presentation.screens.list
 
 import android.annotation.SuppressLint
-import android.util.Log
+import androidx.annotation.StringRes
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.enesk.todocompose.R
-import com.enesk.todocompose.presentation.viewmodel.SharedViewModel
 import com.enesk.todocompose.presentation.ui.theme.fabBackgroundColor
+import com.enesk.todocompose.presentation.viewmodel.SharedViewModel
+import com.enesk.todocompose.util.Action
 import com.enesk.todocompose.util.SearchAppBarState
+import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -38,9 +43,20 @@ fun ListScreen(
     val searchAppBarState: SearchAppBarState by sharedViewModel.searchAppBarState
     val searchTextState: String by sharedViewModel.searchTextState
 
-    sharedViewModel.handleDatabaseActions(action = action)
+    val scaffoldState = rememberScaffoldState()
+
+    DisplaySnackBar(
+        scaffoldState = scaffoldState,
+        handleDatabaseActions = {
+            sharedViewModel.handleDatabaseActions(action)
+        },
+        taskTitle = sharedViewModel.title.value,
+        action = action,
+        stringResource(id = R.string.snack_bar_action_label)
+    )
 
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             ListAppBar(
                 sharedViewModel = sharedViewModel,
@@ -76,5 +92,29 @@ fun FabButtonInListScreen(
             tint = Color.White
         )
 
+    }
+}
+
+@Composable
+fun DisplaySnackBar(
+    scaffoldState: ScaffoldState,
+    handleDatabaseActions: () -> Unit,
+    taskTitle: String,
+    action: Action,
+    actionLabel: String
+) {
+    handleDatabaseActions()
+
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = action) {
+        if (action != Action.NO_ACTION) {
+            scope.launch {
+                val snackBarResult = scaffoldState.snackbarHostState.showSnackbar(
+                    message = "${action.name}: $taskTitle",
+                    actionLabel = actionLabel
+                )
+            }
+        }
     }
 }
