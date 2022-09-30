8194ec9011670b9286c3c4771c8dff07e7595305
kocerenes <eneskocerr41@gmail.com>
2022-09-30 13:57:43 +0300

    Update splash screen

--- app/src/main/java/com/enesk/todocompose/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/enesk/todocompose/presentation/screens/splash/SplashScreen.kt
@@ -1,19 +1,29 @@
 package com.enesk.todocompose.presentation.screens.splash
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.enesk.todocompose.R
 import com.enesk.todocompose.presentation.ui.theme.SPLASH_LOGO_SIZE
 import com.enesk.todocompose.presentation.ui.theme.ToDoComposeTheme
@@ -25,7 +35,20 @@ import kotlinx.coroutines.delay
 fun SplashScreen(
     navigateToListScreen: () -> Unit
 ) {
+    var startAnimation by remember {
+        mutableStateOf(false)
+    }
+    val offsetState by animateDpAsState(
+        targetValue = if (startAnimation) 0.dp else 100.dp,
+        animationSpec = tween(durationMillis = 1000)
+    )
+    val alphaState by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = 1000)
+    )
+
     LaunchedEffect(key1 = true) {
+        startAnimation = true
         delay(SPLASH_SCREEN_DELAY)
         navigateToListScreen()
     }
@@ -38,7 +61,9 @@ fun SplashScreen(
     ) {
         Image(
             modifier = Modifier
-                .size(SPLASH_LOGO_SIZE),
+                .size(SPLASH_LOGO_SIZE)
+                .offset(y = offsetState)
+                .alpha(alpha = alphaState),
             painter = painterResource(id = getLogo()),
             contentDescription = stringResource(id = R.string.todo_logo_description)
         )
