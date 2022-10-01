3e355e189e5c5da0da2402518051ff684f975e5e
kocerenes <eneskocerr41@gmail.com>
2022-10-01 18:21:27 +0300

    Bug fixed for delete at snackbar

--- app/src/main/java/com/enesk/todocompose/presentation/screens/list/ListScreen.kt
+++ app/src/main/java/com/enesk/todocompose/presentation/screens/list/ListScreen.kt
@@ -81,6 +81,7 @@ fun ListScreen(
                 onSwipeToDelete = { action, task ->
                     sharedViewModel.action.value = action
                     sharedViewModel.updateTaskFields(selectedTask = task)
+                    scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
                 },
                 navigateToTaskScreen = navigateToTaskScreen
             )
