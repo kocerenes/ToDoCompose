c4cefb2508447c6bbb511275958345dfc3db5306
kocerenes <eneskocerr41@gmail.com>
2022-10-01 18:39:28 +0300

    Code refactoring

--- app/src/main/java/com/enesk/todocompose/presentation/screens/task/TaskScreen.kt
+++ app/src/main/java/com/enesk/todocompose/presentation/screens/task/TaskScreen.kt
@@ -3,15 +3,10 @@ package com.enesk.todocompose.presentation.screens.task
 import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
-import androidx.activity.OnBackPressedDispatcher
-import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.activity.compose.BackHandler
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalContext
 import com.enesk.todocompose.R
 import com.enesk.todocompose.data.local.entity.ToDoTaskEntity
@@ -32,7 +27,9 @@ fun TaskScreen(
 
     val context = LocalContext.current
 
-    BackHandler(onBackPressed = { navigateToListScreen(Action.NO_ACTION) })
+    BackHandler {
+        navigateToListScreen(Action.NO_ACTION)
+    }
 
     Scaffold(
         topBar = {
@@ -76,28 +73,4 @@ fun displayToast(context: Context) {
         R.string.fields_empty_toast_message,
         Toast.LENGTH_LONG
     ).show()
-}
-
-@Composable
-fun BackHandler(
-    backDispatcher: OnBackPressedDispatcher? =
-        LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
-    onBackPressed: () -> Unit
-) {
-    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
-
-    val backCallback = remember {
-        object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                currentOnBackPressed()
-            }
-        }
-    }
-
-    DisposableEffect(key1 = backDispatcher) {
-        backDispatcher?.addCallback(backCallback)
-        onDispose {
-            backCallback.remove()
-        }
-    }
 }
